7c9a50f05e0f9490f5da5a8facb5fc6e413191e0
Admin <Admin@DESKTOP-IKK7GDI>
2022-08-24 13:28:26 +0530

    JDBC Commit

---
+++ JDBC/src/test/java/JDBC/JDBC/AppTest.java
@@ -0,0 +1,13 @@
+package JDBC.JDBC;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
